177ac0ceab92db258bcf36f14deee2413b4fc0e5
AnayaMody2021 <96385772+AnayaMody2021@users.noreply.github.com>
2023-02-20 10:50:24 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+
+    canvas = createCanvas(500, 500);
+    canvas.position(560,100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('poseNet Is Initionalized!');
+}
+
+function draw() {
+    background('#B76E79')
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+{
+    console.log(results);
+}
+}
